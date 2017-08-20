a3aafc8f046d980790a9e5d9390b0bea68ce1dd5
trabpukcip <trab@icannt.org>
2017-08-21 00:13:48 +0800

    Add all furnace recipes
    -Add all furnace recipes
    -Quartz will be just an ore that is picked up, although I might see if
    the vanilla ore can be overridden

--- src/main/java/org/icannt/netherendingores/common/registry/FurnaceRecipes.java
+++ src/main/java/org/icannt/netherendingores/common/registry/FurnaceRecipes.java
@@ -1,6 +1,7 @@
 package org.icannt.netherendingores.common.registry;
 
 import org.icannt.netherendingores.common.block.metadata.EnumOreModded1Type;
+import org.icannt.netherendingores.common.block.metadata.EnumOreOther1Type;
 import org.icannt.netherendingores.common.block.metadata.EnumOreVanillaType;
 
 import net.minecraft.item.ItemStack;
@@ -11,18 +12,53 @@ import net.minecraftforge.oredict.OreDictionary;
  * Created by ICannt on 20/08/17.
  */
 public class FurnaceRecipes {
+	
 	public static void registerFurnaceRecipes() {
 		
-        for (ItemStack stack : OreDictionary.getOres("oreCoal"))
-        {
-        	stack.setCount(2);
-        	GameRegistry.addSmelting(new ItemStack(BlockRegistry.ORE_NETHER_VANILLA, 1, EnumOreVanillaType.COAL_ORE.ordinal()), stack, 0);
-        }
-        for (ItemStack stack : OreDictionary.getOres("oreCopper"))
-        {
-        	stack.setCount(2);
-        	GameRegistry.addSmelting(new ItemStack(BlockRegistry.ORE_NETHER_MODDED_1, 1, EnumOreModded1Type.COPPER_ORE.ordinal()), stack, 0);
-        }
+		// Vanilla Nether Ores - Furnace
+		for (EnumOreVanillaType variant : EnumOreVanillaType.values()) {			
+	        for (ItemStack stack : OreDictionary.getOres(variant.getFurnaceOreDict()))
+	        {
+	        	stack.setCount(2);
+	        	GameRegistry.addSmelting(new ItemStack(BlockRegistry.ORE_NETHER_VANILLA, 1, variant.ordinal()), stack, 0);
+	        }
+		}
+
+		// Modded Nether Ores 1 - Furnace
+		for (EnumOreModded1Type variant : EnumOreModded1Type.values()) {			
+	        for (ItemStack stack : OreDictionary.getOres(variant.getFurnaceOreDict()))
+	        {
+	        	stack.setCount(2);
+	        	GameRegistry.addSmelting(new ItemStack(BlockRegistry.ORE_NETHER_MODDED_1, 1, variant.ordinal()), stack, 0);
+	        }
+		}
+		
+		// Vanilla End Ores - Furnace
+		for (EnumOreVanillaType variant : EnumOreVanillaType.values()) {			
+	        for (ItemStack stack : OreDictionary.getOres(variant.getFurnaceOreDict()))
+	        {
+	        	stack.setCount(2);
+	        	GameRegistry.addSmelting(new ItemStack(BlockRegistry.ORE_END_VANILLA, 1, variant.ordinal()), stack, 0);
+	        }
+		}
+
+		// Modded End Ores 1 - Furnace
+		for (EnumOreModded1Type variant : EnumOreModded1Type.values()) {			
+	        for (ItemStack stack : OreDictionary.getOres(variant.getFurnaceOreDict()))
+	        {
+	        	stack.setCount(2);
+	        	GameRegistry.addSmelting(new ItemStack(BlockRegistry.ORE_END_MODDED_1, 1, variant.ordinal()), stack, 0);
+	        }
+		}
+		
+		// Other Ores 1 - Furnace
+		// This solution will likely change to just a crafting recipe, with ore dictionary aliases for oreQuartz
+		for (EnumOreOther1Type variant : EnumOreOther1Type.values()) {			
+	        for (ItemStack stack : OreDictionary.getOres(variant.getRecipeOreDict()))
+	        {
+	        	GameRegistry.addSmelting(new ItemStack(BlockRegistry.ORE_OTHER_1, 1, variant.ordinal()), stack, 0);
+	        }
+		}
         
 	}
 }
