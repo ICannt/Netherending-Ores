e0cd0e5bfd5a6a06cce6dafe0a71106e867ed85b
trabpukcip <trab@icannt.org>
2017-09-18 23:00:35 +0800

    Update to 1.0.1
    -Update to 1.0.1, only resources have really changed.

--- src/main/java/org/icannt/netherendingores/NetherendingOres.java
+++ src/main/java/org/icannt/netherendingores/NetherendingOres.java
@@ -25,7 +25,7 @@ public class NetherendingOres {
 
     public static final String MOD_ID = "netherendingores";
     public static final String MOD_NAME = "Netherending Ores";
-    public static final String VERSION = "1.12.1-1.0";
+    public static final String VERSION = "1.12.1-1.0.1";
 
     @SidedProxy(clientSide = "org.icannt.netherendingores.proxies.ClientProxy", serverSide = "org.icannt.netherendingores.proxies.ServerProxy")
     public static CommonProxy proxy;
