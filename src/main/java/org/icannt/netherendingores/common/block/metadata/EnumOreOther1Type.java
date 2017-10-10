aaae8f4e438819f77268155ddcfbf92a6686cfd7
trabpukcip <trab@icannt.org>
2017-10-11 00:38:40 +0800

    Add new ores
    Ardite, Cobalt, Osmium, Yellorite, Uranium and Certus Quartz will all be
    supported in 1.1

--- src/main/java/org/icannt/netherendingores/common/block/metadata/EnumOreOther1Type.java
+++ src/main/java/org/icannt/netherendingores/common/block/metadata/EnumOreOther1Type.java
@@ -9,7 +9,21 @@ import net.minecraft.util.IStringSerializable;
 public enum EnumOreOther1Type implements IStringSerializable {
 	
     OVERWORLD_QUARTZ_ORE ("overworld_quartz_ore", EnumRarity.COMMON, "oreQuartz", "", 0, 0, 3, 15),
-    END_QUARTZ_ORE ("end_quartz_ore", EnumRarity.COMMON, "oreQuartz", "", 0, 0, 3, 15);
+    END_QUARTZ_ORE ("end_quartz_ore", EnumRarity.COMMON, "", "", 0, 0, 3, 15),
+    OVERWORLD_ARDITE_ORE ("overworld_ardite_ore", EnumRarity.COMMON, "", "", 0, 4, 10, 50),
+    OVERWORLD_COBALT_ORE ("overworld_cobalt_ore", EnumRarity.COMMON, "", "", 0, 4, 10, 50),
+    NETHER_CERTUS_QUARTZ_ORE ("nether_certus_quartz_ore", EnumRarity.COMMON, "", "", 0, 0, 3, 15),
+    NETHER_CHARGED_CERTUS_QUARTZ_ORE ("nether_charged_certus_quartz_ore", EnumRarity.COMMON, "", "", 0, 0, 3, 15),
+    NETHER_OSMIUM_ORE ("nether_osmium_ore", EnumRarity.COMMON, "", "", 0, 0, 3, 15),
+    NETHER_URANIUM_ORE ("nether_uranium_ore", EnumRarity.COMMON, "", "", 0, 2, 4, 30),
+    NETHER_YELLORITE_ORE ("nether_yellorite_ore", EnumRarity.COMMON, "", "", 0, 0, 2, 10),
+    END_ARDITE_ORE ("end_ardite_ore", EnumRarity.COMMON, "", "", 0, 0, 10, 50),
+    END_CERTUS_QUARTZ_ORE ("end_certus_quartz_ore", EnumRarity.COMMON, "", "", 0, 0, 3, 15),
+    END_CHARGED_CERTUS_QUARTZ_ORE ("end_charged_certus_quartz_ore", EnumRarity.COMMON, "", "", 0, 0, 3, 15),
+    END_COBALT_ORE ("end_cobalt_ore", EnumRarity.COMMON, "", "", 0, 4, 10, 50),
+    END_OSMIUM_ORE ("end_osmium_ore", EnumRarity.COMMON, "", "", 0, 0, 3, 15),
+    END_URANIUM_ORE ("end_uranium_ore", EnumRarity.COMMON, "", "", 0, 2, 4, 30),
+    END_YELLORITE_ORE ("end_yellorite_ore", EnumRarity.COMMON, "", "", 0, 0, 2, 10);
 
     private String name;
     private EnumRarity rarity;
