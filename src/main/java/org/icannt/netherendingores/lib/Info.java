98c643ee5a0ac8f4de7614f520cc3ef985f8d01a
trabpukcip <trab@icannt.org>
2019-03-17 12:34:52 +0800

    Change to 1.4, Add lang strings & ore textures
    -Change version to 1.4 for further development
    -Add lang stringhs and names for new ores
    -Add all textuires for new ores, may be subject to future revision, closes #15, closes #16

--- src/main/java/org/icannt/netherendingores/lib/Info.java
+++ src/main/java/org/icannt/netherendingores/lib/Info.java
@@ -9,7 +9,7 @@ public class Info {
     public static final String MOD_ID = "netherendingores";
     public static final String MOD_NAME = "Netherending Ores";
     
-    public static final String VERSION = "1.12.2-1.3";
+    public static final String VERSION = "1.12.2-1.4";
     public static final String MC_VERSIONS = "[1.12.2,1.13)";
     public static final String CFG_VERSION = "1.1";
     
