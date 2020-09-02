dcfac91e0f4dc5c3a4b1b5915faceddaf3477b3b
trabpukcip <trab@icannt.org>
2020-09-02 20:37:50 +0800

    Fix For Pick Block Item Drop Crash
    Implemented an override for block picking to stop crashes when using item drops.
    
    Closes #25

--- src/main/java/org/icannt/netherendingores/common/block/blocks/BlockOreVariantBase.java
+++ src/main/java/org/icannt/netherendingores/common/block/blocks/BlockOreVariantBase.java
@@ -11,8 +11,11 @@ import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.Explosion;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
@@ -97,6 +100,14 @@ public class BlockOreVariantBase extends Block {
     	
     	return 0;
     	
+    }
+    
+    @Override
+    public ItemStack getPickBlock(IBlockState state, RayTraceResult target, World world, BlockPos pos, EntityPlayer player)
+    {
+    	
+        return new ItemStack(Item.getItemFromBlock(this), 1, getMetaFromState(state));
+        
     }
     
     @Override
--- src/main/java/org/icannt/netherendingores/lib/Info.java
+++ src/main/java/org/icannt/netherendingores/lib/Info.java
@@ -9,7 +9,7 @@ public class Info {
     public static final String MOD_ID = "netherendingores";
     public static final String MOD_NAME = "Netherending Ores";
     
-    public static final String VERSION = "1.12.2-1.4.1";
+    public static final String VERSION = "1.12.2-1.4.2";
     public static final String MC_VERSIONS = "[1.12.2,1.13)";
     public static final String CFG_VERSION = "1.2.1";
     
